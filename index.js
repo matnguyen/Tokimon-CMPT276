7cf98c689ce7315980bef47f1e03273ebe9c8397
Matthew Nguyen <matthewnguyen.667@gmail.com>
2019-10-06 21:38:16 -0700

    Add pg database

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .engine('html', require('ejs').renderFile)
   .set('view engine', 'html')
   .get('/', (req, res) => res.render('pages/calculator'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
