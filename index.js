845b289da9dd9687f0c2f5f065ed8790d21524d2
Matthew Nguyen <matthewnguyen.667@gmail.com>
2019-10-08 21:48:12 -0700

    rename files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var app = express();
 
 const { Pool } = require('pg');
 const pool = new Pool({
@@ -8,23 +9,22 @@ const pool = new Pool({
   ssl: true
 });
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .engine('html', require('ejs').renderFile)
-  .set('view engine', 'html')
-  .get('/', (req, res) => res.render('pages/main'))
-  .set('view engine', 'ejs')
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM Tokimon');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+  // .engine('html', require('ejs').renderFile)
+app.set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/main'))
+  // .get('/db', async (req, res) => {
+app.get('/', async (req, res) => {
+      try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM Tokimon');
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('pages/main', results );
+        client.release();
+      } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+      }
+    })
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
